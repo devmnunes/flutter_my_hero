07d8e8f9852f609cc883583be4dccdd8b179e4f6
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-03-10 20:37:55 -0300

    atualização

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_my_hero/data/my_character.dart';
 import 'package:flutter_my_hero/models/character.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,33 +11,47 @@ class HomeScreen extends StatelessWidget {
     Character character = getMyCharacter();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meu Personagem",
-         style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
+        title: Text(
+          "Meu Personagem",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
         ),
         backgroundColor: const Color.fromARGB(255, 36, 128, 39),
         centerTitle: true,
         shadowColor: Colors.black,
         elevation: 7,
-        ),
-        floatingActionButton: FloatingActionButton(onPressed:() {
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
           print('O BOTÃO FOI CLICADO');
         },
         backgroundColor: const Color.fromARGB(255, 36, 128, 39),
         child: Icon(
-          Icons.add, 
+          Icons.add,
           color: Colors.white,
-          ), 
         ),
-        body: Column(
-          children: [
-            Image.network(character.urlImage!,
-             width: 300,
-             ),
-          ],
-        ),
-      );
-   }
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.network(
+              character.urlImage!,
+              width: 300,
+            ),
+          ),
+          Text(
+            character.name,
+            style: GoogleFonts.seaweedScript(
+              fontSize: 62,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+              "Por ${character.player}, criado em ${character.createdAt.toString().substring(0, 10)}")
+        ],
+      ),
+    );
+  }
 }
