541c3ed9dd6d1ad2f2748eb9fa829a54ee6207f4
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-03-18 21:11:32 -0300

    Update minha pag

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_hero/screens/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatelessWidget {
-  const Login ({super.key});
-  
+  const Login({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,30 +13,33 @@ class Login extends StatelessWidget {
         resizeToAvoidBottomInset: false,
         body: Container(
           color: const Color.fromARGB(255, 36, 128, 39),
-            child: Center(
-              child: Column(children: [
-              SizedBox(
-                height: 129,
-                child: Padding(
-                  padding: const EdgeInsets.all(9.0),
-                  child: Image.asset('lib/image/logo.png'),
-                )
-              ),
-              Text(
-                "Entre na sua conta Street Fighter.",
-                style: GoogleFonts.tektur(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-                  SizedBox( height: 20,),
-                  Container(
-                  width: 180,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(40, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(8.5)
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                    height: 129,
+                    child: Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: Image.asset('lib/image/logo.png'),
+                    )),
+                Text(
+                  "Entre na sua conta Street Fighter.",
+                  style: GoogleFonts.tektur(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: Column(
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  height: 180,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(40, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(8.5)),
+                  child: const Column(
                     children: [
                       TextField(
                         decoration: InputDecoration(
@@ -44,12 +48,13 @@ class Login extends StatelessWidget {
                           hintStyle: TextStyle(color: Colors.white),
                           icon: Icon(
                             Icons.email_outlined,
-                             color: Colors.white,
-                             ),    
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                      SizedBox(height: 20,),
-
+                      SizedBox(
+                        height: 20,
+                      ),
                       Row(
                         children: [
                           Flexible(
@@ -61,35 +66,58 @@ class Login extends StatelessWidget {
                                 hintStyle: TextStyle(color: Colors.white),
                                 icon: Icon(
                                   Icons.key_outlined,
-                                   color: Colors.white,
-                                ),    
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
                           Icon(
                             Icons.visibility_outlined,
-                             color: Colors.white,
+                            color: Colors.white,
                           )
                         ],
                       ),
                     ],
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                Text('Esqueceu a senha?',
-                 style: TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.bold),
+                SizedBox(
+                  height: 35,
                 ),
-                Container(
-                  width: 200,
-                  height: 80,
-                  color: Colors.blue,
-                )
-                
-           ],
-           ),
+                Row(children: [
+                  Text(
+                    'Esqueceu a senha?',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  const Spacer(),
+                  Container(
+                    width: 120,
+                    height: 70,
+                    decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomeScreen()),
+                          );
+                        },
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
           ),
         ),
       ),
